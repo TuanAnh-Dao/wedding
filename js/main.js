6b7fc3e495f2a359669907de8d0ed3d5fd92756d
TuanAnh-Dao <108260945+TuanAnh-Dao@users.noreply.github.com>
2024-06-09 14:12:17 +0900

    Test2

--- js/main.js
+++ js/main.js
@@ -270,7 +270,16 @@
 	
 				
 				fetch(scriptURL, { method: "POST", 
-				body: getFormData(form)})
+				 mode: "cors", // no-cors, *cors, same-origin
+					cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+					credentials: "omit", // include, *same-origin, omit
+					headers: {
+					"Content-Type": "application/json",
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+					},
+					redirect: "follow", // manual, *follow, error
+					referrerPolicy: "no-referrer",
+				body: JSON.stringify(getFormData(form))})
 					.then((response) => {
 						console.log(response);
 					btn.disabled = false;
